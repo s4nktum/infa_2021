a84c0e17228de076f7f5f3023af62723550f2f83
s4nktum <irnbru382@gmail.com>
2021-05-17 01:51:24 +0300

    Сочинил реализацию Нужно сделать еще 3 функции для основания, стен, крыши

--- lab4/house.py
+++ lab4/house.py
@@ -4,6 +4,12 @@ from pygame import *
 pygame.init()
 
 
+def main():
+    x, y = 300, 200
+    width, height = 200, 300
+    draw_house(x, y, width, height)
+
+
 def draw_house(x, y, width, height):
     """
     Нарисовать домик ширины width и высоты height от опорной точки (x, y), которая нахоится
@@ -14,13 +20,43 @@ def draw_house(x, y, width, height):
     :param height: полная высота домика
     :return: None
     """
-    print(x, y, width, height)
+
+    print('Типа рисую домик', x, y, width, height)
+    foundation_height = 0.05 * height
+    walls_width = 0.9 * width
+    walls_height = 0.5 * height
+    roof_height = height - foundation_height - walls_height
+
+    draw_house_foundation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
+
+
+def draw_house_foundation(x, y, width, height):
+    print('Типа рисую основание', x, y, width, height)
+    """
+    Нарисовать основание домика ширины width и высоты height от опорной точки (x, y), которая нахоится
+    в середине нижней точки фундамента.
+    :param x: координата x середины фундамента
+    :param y: координата y низа фундамента
+    :param width: полная ширина фундамента
+    :param height: полная высота фундамента
+    :return: None
+    """
+    print('Типа рисую основание', x, y, width, height)
     pass
 
 
-x, y = 300, 200
-width, height = 200, 300
+def draw_house_walls(x, y, width, height):
+    print('Типа рисую стены', x, y, width, height)
+    pass
 
 
-draw_house(x, y, width, height)
+def draw_house_roof(x, y, width, height):
+    print('Типа рисую крышу', x, y, width, height)
+    pass
+
+
+main()
+
 
